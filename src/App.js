eb2e81bf9ed09c95169638c9a2ad58574d460f6b
Luis Hurtado <luisferhc352@gmail.com>
2022-06-21 11:54:51 -0500

    app component with route and styles added

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import { Routes, Route, Link } from 'react-router-dom';
+import TaskList from './components/taskList/taskList';
+import CreateTask from "./components/createTask/createTask";
+import TaskDetail from './components/taskDetail/taskDetail';
+import "./App.css"
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
 
+   return(
+     <main>
+        <nav className='navTask' >
+          <Link to="/" >Home</Link>
+          <Link to="/tasks">Tasks</Link>
+        </nav>
+        <article className='mainContainer' >
+          <Routes>
+            <Route 
+              path='/' 
+              element={<CreateTask/>}
+            />
+            <Route path='/tasks' element={<TaskList/>}/>
+            <Route path='/tasks/:id' element={<TaskDetail/>}/>            
+          </Routes>
+        </article>
+     </main>
+   )
+} 
 export default App;
